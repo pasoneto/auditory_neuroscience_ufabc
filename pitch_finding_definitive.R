7625a9478e7e21d32d6c8db47354bbc5fbe39ad5
pasoneto <pasoneto@gmail.com>
2020-05-16 17:13:53 -0300

    srgs

--- pitch_finding_definitive.R
+++ pitch_finding_definitive.R
@@ -1,8 +1,7 @@
 library(soundgen)
 library(seewave)
 
-setwd('C:/Users/Lenovo/Desktop/world/Ciência/UFABC/Matérias/Auditory Neuroscience/audios_pavao')
-
+setwd('C:/Users/Lenovo/Desktop/teste')
 
 #https://cran.r-project.org/web/packages/soundgen/vignettes/acoustic_analysis.html#missing-fundamental
 pitch_finding <- function(pasta){
@@ -14,6 +13,7 @@ pitch_finding <- function(pasta){
   for(i in pasta){
       analise = analyze(i, plot = FALSE,
                   pitchMethods = c('autocor', 'cep', 'spec'),
+                  specPeak = 1,
                   nCands = 1)
       autocor[[i]] = median(analise$pitchAutocor, na.rm = TRUE)
       cepstral[[i]] = median(analise$pitchCep, na.rm = TRUE)
@@ -32,11 +32,4 @@ pitch_finding <- function(pasta){
 }
 
 oi = pitch_finding(list.files())
-analise = analyze(list.files()[1],
-                  samplingRate = 22050,
-                  entropyThres = 1,
-                  plot = TRUE,
-                  pitchMethods = c('autocor', 'cep', 'spec'),
-                  nCands = 3)
 
-print(list.files())

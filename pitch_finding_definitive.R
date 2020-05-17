bcf0955eb8cb87a06674a8f67bfb8243c4607fbd
pasoneto <pasoneto@gmail.com>
2020-05-16 22:13:21 -0300

    Update pitch_finding_definitive.R

--- pitch_finding_definitive.R
+++ pitch_finding_definitive.R
@@ -1,20 +1,21 @@
 library(soundgen)
 library(seewave)
-
-setwd('C:/Users/Lenovo/Desktop/teste')
-
 #https://cran.r-project.org/web/packages/soundgen/vignettes/acoustic_analysis.html#missing-fundamental
+
+#Function receives a list of audio files and returns a data
+#frame with pitch analysis for each file
 pitch_finding <- function(pasta){
 
   autocor = c()
   cepstral = c()
   spectral = c()
 
-  for(i in pasta){
-      analise = analyze(i, plot = FALSE,
+  for(i in 1:length(pasta)){
+      audio = tuneR::readWave(pasta[i])
+      analise = analyze(audio@left, plot = FALSE,
                   pitchMethods = c('autocor', 'cep', 'spec'),
-                  specPeak = 0,
-                  nCands = 1)
+                  samplingRate = audio@samp.rate
+                  )
       autocor[[i]] = median(analise$pitchAutocor, na.rm = TRUE)
       cepstral[[i]] = median(analise$pitchCep, na.rm = TRUE)
       spectral[[i]] = median(analise$pitchSpec, na.rm = TRUE)
@@ -26,10 +27,20 @@ pitch_finding <- function(pasta){
   
   data_final = dplyr::bind_rows(c(autocor, cepstral, spectral))
   #adding name to files
-  data_final$file_names = pasta
+  data_final$file_names = pasta[i]
   
   return(data_final)
 }
 
-oi = pitch_finding(list.files())
+#Setting path
+caminho = 'C:/Users/Lenovo/Desktop/world/Ciência/UFABC/Matérias/Auditory Neuroscience/audio/'
+setwd(caminho)
+folder = list.files(path = caminho) 
 
+#Guardando arquivos para cada frequencia
+arquivos_finals = c()
+for(i in 1:length(folder)){
+  setwd(paste(caminho, folder[i], sep=''))
+  pasta = list.files(path = paste(caminho, folder[i], sep=''))
+  arquivos_finals[[i]] = pitch_finding(pasta)  
+}
